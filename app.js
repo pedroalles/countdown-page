ad4ecc8a0cba123ab0a65516bd6cf090c5cbb628
Pedro Alles <pedroalles@gmail.com>
2021-05-09 23:03:22 -0300

    Change countdown date

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Dec 03, 2021 00:00:00").getTime()
+    const countDate = new Date("May 27, 2021 00:00:00").getTime()
     const now = new Date().getTime()
     const gap = countDate - now
 
